fbf691a55aacf04fd477cf70103ec7e771fecb3d
Jan du Plessis <drjanduplessis@icloud.com>
2024-06-29 19:32:21 +0100

    Automated commit via Python script - 2024-06-29 19:32

--- money_mate/streamlit_app.py
+++ money_mate/streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import streamlit_shadcn_ui as ui
+import pandas as pd
+import numpy as np
+import altair as alt
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import altair as alt
+
+from money_mate.utils import *
+
 
 # Set the page configuration to have a wide layout and the sidebar collapsed on load
 st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
@@ -13,15 +22,25 @@ def check_passcode():
         if entered_passcode == passcode:
             st.session_state["authenticated"] = True
         else:
-            ui.badges(badge_list=[("Incorrect Passcode, please try again.", "destructive")], class_name="flex gap-2", key="error1")
+            st.write()
+            st.write()
+            ui.badges(badge_list=[("Incorrect Passcode, please try again.", "default")], class_name="flex gap-2", key="error1")
+            st.toast(body="Incorrect Password", icon="‼️")
 
 # Check if the user is authenticated
 if "authenticated" not in st.session_state:
     st.session_state["authenticated"] = False
 
 if not st.session_state["authenticated"]:
-    st.markdown(f"# ![Protected](https://img.icons8.com/pastel-glyph/64/fingerprint.png) Protected Content")
-    check_passcode()
+        c1, c2, c3 = st.columns(3)
+
+        with c2:
+            #st.image("images/private.png")
+            st.image("images/private_flat.png")
+            st.markdown(f"# ![Protected](https://img.icons8.com/pastel-glyph/64/fingerprint.png) Protected Content")
+            check_passcode()
+
+
 else:
     st.markdown("# ![Money-Mate](https://img.icons8.com/dotty/80/coins.png) Money-Mate")
 
@@ -45,36 +64,117 @@ else:
         worksheet="Personal Account Transactions",
         ttl="10m",
     )
+    data = prep_account_statement(data)
 
-    budget = conn.read(
+    regular_expenses = conn.read(
         worksheet="Budget",
         ttl="10m",
     )
+    budget = prep_budget(regular_expenses)
+    filtered_bank_statement, days_remaining = prep_statement_import_to_budget(data)
+    current = generate_budget_df(filtered_bank_statement, budget)
+    # Return Budget Metrics
+    total_budget, income_value, budget_used_sum, over_spent, remaining_budget, daily_budget, projected_disposable_income, actual_disposable_income = prep_budget_metrics(current, days_remaining)
+    current_minus_income = budget_df_min_income(current)
+
+
+
+
+
+
 
     if tabs == "Account Summary":
-        pass
+        st.subheader("Account Summary")
+        st.toast(body="""**Account Summary** - Monzo Bank""", icon="💷")
+
 
     elif tabs == "Budget":
-        pass
+        st.subheader("Budget")
+        st.toast(body="""**Monthly Budget** to date.""", icon="😀")
+
+        c1, c2, c3, c4 = st.columns(4)
+        c1.metric(label="Total Monthly Budget", value=str(total_budget))
+        c2.metric(label="Budget Used this Month", value=str(budget_used_sum))
+        c3.metric(label="Remaining Allocation", value=str(remaining_budget))
+        c4.metric(label="Anticipated Surplus", value=str(projected_disposable_income))
+
+        # Create the horizontal bar plot
+        plt.figure(figsize=(20, 6), dpi=300)
+        colors = ['#ad2c6a' if val >= 0 else '#f3bd66' for val in current_minus_income['Diff']]
+
+        bars = plt.barh(current_minus_income['categories'], current_minus_income['Diff'], color=colors)
+
+        # Add data labels inside the bars
+        for bar in bars:
+            plt.text(
+                bar.get_width() if bar.get_width() < 0 else bar.get_width() + 1,
+                bar.get_y() + bar.get_height() / 2,
+                f'{bar.get_width():.2f}',
+                va='center',
+                ha='right' if bar.get_width() < 0 else 'left',
+                color='black'
+            )
+
+        # Set labels and title
+        plt.xlabel('Difference')
+        plt.ylabel('Categories')
+        plt.title('Budget Difference by Category')
+        plt.gca().invert_yaxis()  # To display the highest values at the top
+
+        # Remove borders
+        plt.gca().spines['top'].set_visible(False)
+        plt.gca().spines['right'].set_visible(False)
+        plt.gca().spines['bottom'].set_visible(False)
+        plt.gca().spines['left'].set_visible(False)
+        # Add grid with specified linewidth and alpha
+        plt.grid(True, linewidth=0.5, alpha=0.6)
+        plt.tight_layout()
+        st.pyplot(plt)
+
+
+
+        c1, c2, c3, c4 = st.columns(4)
+        c1.metric(label="Over Budget to date", value=str(over_spent))
+        c2.metric(label="Realized Surplus", value=str(actual_disposable_income))
+        c4.metric(label="Remaining Daily Budget", value=str(daily_budget))
+
+        st.divider()
+        switch_value1 = ui.switch(default_checked=False, label="Show Budget Calculation", key="switch1")
+        if switch_value1:
+            st.dataframe(current_minus_income)
+        switch_value2 = ui.switch(default_checked=False, label="Show Bank Statement", key="switch2")
+        if switch_value2:
+            st.dataframe(filtered_bank_statement)
 
     elif tabs == "Account History":
-        pass
+        st.subheader("Account History")
+        st.toast(body="""Viewing **Income & Expenses History**""", icon="📈")
 
     elif tabs == "View | Update - Budget":
-        with st.expander(label="Budget - DataFrame"):
+        st.subheader("Budget Documents")
+        st.toast(body="""Viewing **Budget DataFrames**""", icon="🔢")
+        with st.expander(label="Budget - DataFrame", icon="🔢"):
             st.dataframe(budget)
 
-        with st.expander(label="Budget - Google Sheet"):
+        with st.expander(label="Budget prepped with Statement Info - DataFrame", icon="🔢"):
+            st.dataframe(current)
+
+        with st.expander(label="Budget - Google Sheet", icon="📋"):
             # Embed Google Sheet in an expander
             google_sheet_url = "https://docs.google.com/spreadsheets/d/1bpW10hRPxTDwQ1UjEBsKLKwZ9tf9RTFl91GQhVv1luI/edit?gid=1571834654#gid=1571834654"
             st.components.v1.html(f'<iframe src="{google_sheet_url}" width="100%" height="600"></iframe>', height=600)
 
 
     elif tabs == "View | Update - Bank Statement":
-        with st.expander(label="Account Statement - DataFrame"):
+        st.subheader("Bank Statements")
+        st.toast(body="""Viewing **Bank Statements - Dataframes**""", icon="🔢")
+        with st.expander(label="Account Statement - DataFrame", icon="🔢"):
             st.dataframe(data)
 
-        with st.expander(label="Account Statement - Google Sheet"):
+        with st.expander(label="Account Statement for Budget - DataFrame", icon="🔢"):
+            st.dataframe(filtered_bank_statement)
+
+        with st.expander(label="Account Statement - Google Sheet", icon="📋"):
             # Embed Google Sheet in an expander
             google_sheet_url = "https://docs.google.com/spreadsheets/d/1bpW10hRPxTDwQ1UjEBsKLKwZ9tf9RTFl91GQhVv1luI/edit?usp=sharing"
             st.components.v1.html(f'<iframe src="{google_sheet_url}" width="100%" height="600"></iframe>', height=600)
--- money_mate/utils.py
+++ money_mate/utils.py
@@ -1,5 +1,7 @@
 import time
 from loguru import logger
+import pandas as pd
+from datetime import datetime, timedelta
 
 # = Decorators =========================================================================================================
 
@@ -19,3 +21,344 @@ def time_it(func):
 
 
 # Universal functions ==================================================================================================
+mapping = {
+    "Stuff": [
+        "Amazon",
+        "Amazon Music",
+        "Audible",
+        "Amazon",
+        "Clonezone",
+        "Clonezone - Soho",
+        "G-star Raw",
+        "GSingh",
+        "IKEA",
+        "Leyland Lsdm Earl's Court",
+        "Leyland SDM",
+        "Robert Dyas",
+        "United Shop Company",
+        "eBay",
+        "www.thickwall.co.uk",
+        "World Duty Free",
+        "Apple Store",
+    ],
+    "Travel": [
+        "TIER",
+        "Tier Mobility",
+        "Sendwave",
+        "Zipcar",
+        "Tier Mobility",
+        "Lime",
+        "Transport for London",
+        "CMT UK Taxi Fare",
+        "Uber",
+        "Easi Rent",
+        "easirent",
+        "London Taxi",
+    ],
+    "Subscriptions": [
+        "Wordtune",
+        "Automate.io",
+        "Coursera",
+        "Rytr - Ai Writer",
+        "Netflix",
+        "Zapier",
+        "Notion2shee",
+        "Nutt Labs + Notion Vip",
+        "Google",
+        "Yourdataltd",
+        "Airtable.com/bill",
+        "Pipedream, Inc.",
+        "Superhuman",
+        "Fs Revoicer",
+        "Heroku",
+        "Deep Learning Courses",
+        "Render.com",
+        "Setapp",
+        "Spark",
+        "Nutt Labs + Notion Vip",
+        "Motion ",
+        "Browserless",
+        "www.make.com",
+        "Claude",
+        "WOW Presents PLUS",
+        "Microsoft",
+        "Loom Subscription",
+        "Amazon Prime",
+        "Super Publishing Co.",
+        "Apple",
+        "Perplexity",
+        "Notion",
+        "OpenAI",
+        "Monzo Premium",
+        "Anthropic",
+        "Motion -1 Temp Hold",
+        "Brompton Super",
+        "ExpressVPN",
+        "Grammarly",
+        "Granity-ent.com",
+        "Heart Internet",
+        "Jarvis - Conversion.ai",
+        "Realpython",
+        "Surfshark",
+        "PureGym",
+    ],
+    "Groceries": [
+        "Jms Food Store",
+        "J M S Foods",
+        "Sunfield Foods ",
+        "Amazon Fresh",
+        "Lidl",
+        "Waitrose & Partners",
+        "M&S",
+        "Tesco",
+        "London Taxi 77619",
+        "Sainsbury’s",
+        "Co-op",
+        "Deepak Self Service",
+        "Chelsea Food Fayre",
+        "Chelsea Food Fayre",
+        "Chelsea Food Worldsend",
+        "Cumberland Food & Wine",
+        "Earls Court Food And Wine",
+        "J M S Food & News",
+        "My Shop",
+        "On The Go",
+        "Chelsea Food And Wine",
+    ],
+    "Eating Out": [
+        "Taco Bell",
+        "Paul Uk Hammersmith",
+        "John Forrest Master Ba",
+        "Five Guys",
+        "Mona Lisa Cafe",
+        "Wagamama",
+        "Balans West",
+        "Ollie?s House Limited",
+        "The Monument",
+        "Pret",
+        "Nando’s",
+        "The Grove Tavern",
+        "Balans West",
+        "Yoco Pickled Green",
+        "Star Wraps",
+        "Bagel Bakery Bar",
+        "Greggs",
+        "PAUL Earls Court",
+        "Lions Prep",
+        "Patri Takeaway",
+        "Deliveroo",
+        "Leon",
+        "Black Rabbit Cafe",
+        "GAIL's Bakery",
+        "McDonald’s",
+        "Starbucks",
+        "Buff Meat",
+        "Burger King",
+        "Domino’s",
+        "KFC",
+        "Masala",
+        "PAUL Hammersmith",
+        "Pho",
+        "The Bull Westfield",
+        "The Grove",
+        "Too Good To Go",
+        "Coffee",
+        "The Swan",
+        "Bayswater Arms",
+        "Soft Ice",
+        "Old Ship, Hammersmith",
+        "Riverside Studio",
+    ],
+    "Telephone": [
+        "Airalo",
+        "TELSERVE LIMITED",
+        "EE",
+        "plan.com",
+        "giffgaff",
+    ],
+    "Rent": ["Hampton Management", "Hampton Rent"],
+    "Smoking": [
+        "On The Goo",
+        "The Smoking Jacket",
+        "Cheyne News",
+        "Deepak Self Service",
+        "Lucky Me Enterprise",
+        "smoking",
+    ],
+    "Debt Payment": [
+        "HMRC",
+        "Credit Resource Solutions",
+        "Flex",
+    ],
+    "Medical": [
+        "Medicine Chest",
+        "Boots",
+        "NHS Prescription Prepayment",
+        "Green Light Pharmacy",
+        "Earls Court Chemist",
+        "Superdrug",
+        "Zafash Pharmacy",
+    ],
+    "Other": [
+        "James Vokins",
+        "Empriel",
+        "PayPal",
+        "Rm Media",
+        "Leatherpr",
+        "Michele Manzolillo",
+        "Dipanno Dario",
+        "Jan Du plessis revolut",
+        "Alessandro Dei Agnoli",
+        "Skrill9959",
+        "James Vockins",
+        "kucoin.com",
+        "Patreon",
+        "Cash App",
+        "Wishtender.com",
+        "Skrill3395",
+        "Jan Du plessis revolut",
+        "Coinbase Payments",
+        "Coinbase",
+        "ATM",
+        "carlos",
+        "Kostadin Milchev",
+        "Revolut",
+        "coinbase",
+        "Koronapay Europe",
+        "A Garcia",
+        "Cornelioallanj",
+
+    ],
+    "Credit Cards": [
+        "Capital One",
+        "Vanquis Bank",
+    ],
+    "Income": [
+        "GOOD RESEARCH LTD",
+        "DR BURHAN ALI ADIB AND DR ORIETTA E",
+        "DU PLESSIS J V B",
+        "- GOOD RESEARCH LT",
+        "EARLS CT SUR",
+        "Atlantic Medical",
+    ],
+    "Savings + Transfer": [
+        "LE-TELLIER J",
+        "Jan du Plessis",
+        "50p Savings  Pot",
+        "Jan Du Plessis",
+        "Savings Pot",
+        "Jan Van-Breda-Du-P",
+        "Jan Van Breda Du P",
+        "Jean-Pierre LE TELLIER",
+        "Complete Savings",
+
+        "Plum",
+    ],
+    "Holiday": [
+        "Airbnb",
+        "Booking.com",
+        "Bootlegger Cape Quarte",
+        "The Grey Hotel",
+        "Wise Holiday",
+        "Woodford Car Hire",
+    ],
+    "Barber": [
+        "Sw5 Barbers Lt",
+        "Cut And Go",
+        "Old Brompton Barbers",
+        "Sw5 Barbers Lt",
+    ],
+    "Bank Charges": [
+        "Bank Charges",
+    ],
+    "Cows+LandSA": [
+        "Julie Young",
+        "thom",
+        "Myra Cosio"
+    ],
+
+}
+
+def categorize(name):
+    name = name.lower()
+    for category, names in mapping.items():
+        for n in names:
+            if n.lower() in name:
+                return category
+    return "Uncategorized"
+
+
+def prep_account_statement(df):
+    df.drop(columns=["Transaction ID", "Category split", "Receipt"], inplace=True)
+    df["Date"] = pd.to_datetime(df["Date"], dayfirst=True)
+    df["year"] = df["Date"].dt.year
+    df["month"] = df["Date"].dt.month
+
+    df["categories"] = df["Name"].apply(categorize)
+    df["Cumulative Amount"] = df["Amount"].cumsum().round(2)
+
+    return df
+
+def prep_budget(df):
+    budget = df.groupby(by="Category")["Amount"].sum().reset_index()
+    budget.columns = ["categories", "Budget"]
+
+    return budget
+
+
+def prep_statement_import_to_budget(df):
+    # Convert the 'date' column to datetime if it isn't already
+    df["Date"] = pd.to_datetime(df["Date"])
+
+    # Get the current date
+    current_date = datetime.now()
+
+    # Calculate the start date for tracking (24th of the previous month)
+    if current_date.day >= 24:
+        start_date = current_date.replace(day=24)
+    else:
+        # If the current day is less than 24, go to the previous month
+        start_date = (current_date - pd.DateOffset(months=1)).replace(day=24)
+
+    # Filter the dfFrame to get rows from the start date onwards
+    filtered_bank_statement = df[df["Date"] >= start_date]
+
+
+    # Calculate the number of days remaining until the 24th of the current month
+    next_24th = current_date.replace(day=25)
+    if current_date.day > 25:
+        next_24th = (current_date + pd.DateOffset(months=1)).replace(day=24)
+    days_remaining = (next_24th - current_date).days
+
+    return filtered_bank_statement, days_remaining
+
+
+def generate_budget_df(filtered_bank_statement, budget):
+    current_expenses_by_cat = filtered_bank_statement.groupby(by='categories')['Amount'].sum().abs().reset_index()
+    current = budget.merge(current_expenses_by_cat, how='left', on='categories').fillna(0)
+    current['Diff'] =  current['Budget'] - current['Amount']
+
+    return current
+
+
+def budget_df_min_income(current):
+    current_minus_income = current[current["categories"] != "Income"]
+
+    return current_minus_income
+
+
+def prep_budget_metrics(current, days_remaining):
+    total_budget = abs(current['Budget'].sum().round(2))
+    income_value = abs(current.loc[current["categories"] == "Income", "Diff"].values[0])
+    current_minus_income = budget_df_min_income(current)
+    budget_used_sum = current_minus_income["Amount"].sum().round(2)
+    remaining_budget = current_minus_income["Diff"].sum().round(2)
+    over_spent = (
+        current_minus_income[current_minus_income["Diff"] < 0]["Diff"].sum().round(2)
+    )
+    daily_budget = (remaining_budget / days_remaining).round(2)
+
+    projected_disposable_income = (income_value - total_budget).round(2)
+    actual_disposable_income = ((income_value - total_budget) - abs(over_spent)).round(2)
+
+    return total_budget, income_value, budget_used_sum, over_spent, remaining_budget, daily_budget, projected_disposable_income, actual_disposable_income
