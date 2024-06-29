9e66b46265b99974d64a3ced4be54a37ff2e2f7a
Jan du Plessis <drjanduplessis@icloud.com>
2024-06-29 11:53:08 +0100

    Automated commit via Python script - 2024-06-29 11:53

--- money_mate/streamlit_app.py
+++ money_mate/streamlit_app.py
@@ -1,1 +1,23 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+
+st.sidebar.header("Money-Mate")
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+data = conn.read(
+    worksheet="Personal Account Transactions",
+    ttl="10m",
+)
+
+budget = conn.read(
+    worksheet="Budget",
+    ttl="10m",
+)
+
+st.header("Money-Mate")
+st.dataframe(data)
+st.dataframe(budget)
