496ae302fcb41195d7dfe8d5de90a2a91ca73f7a
Jan du Plessis <drjanduplessis@icloud.com>
2024-06-30 01:19:36 +0100

    Automated commit via Python script - 2024-06-30 01:19

--- money_mate/streamlit_app.py
+++ money_mate/streamlit_app.py
@@ -11,6 +11,7 @@ import altair as alt
 from utils import *
 
 
+
 # Set the page configuration to have a wide layout and the sidebar collapsed on load
 st.set_page_config(layout="wide", initial_sidebar_state="collapsed", page_title="Money-Mate")
 st.logo('images/mmlogo.png')
