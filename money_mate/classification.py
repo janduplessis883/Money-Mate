954bf1ac5463f512e3da6d3db0d188659cfc2750
Jan du Plessis <drjanduplessis@icloud.com>
2024-07-08 23:11:10 +0100

    Automated commit via Python script - 2024-07-08 23:11

--- money_mate/classification.py
+++ money_mate/classification.py
@@ -4,7 +4,7 @@ import streamlit as st
 import toml
 
 # Load Setting via settings.toml file
-def load_settings(file_path="settings.toml"):
+def load_settings(file_path="../settings.toml"):
     with open(file_path, "r") as file:
         settings = toml.load(file)
     return settings
@@ -471,6 +471,7 @@ startswith_rules = {
         "Tier",
         "Taxi",
         "Uber",
+        "Zipcar",
     ],
     "Eating Out": [
         "Toogoodt",
